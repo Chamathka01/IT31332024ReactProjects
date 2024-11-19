d0ebdf4cf20e6cf25214bdf81d7455c22f68c251
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-19 14:05:18 +0000

    get data from parent and print using map

--- myapp/src/components/StudentTable.js
+++ myapp/src/components/StudentTable.js
@@ -12,20 +12,26 @@ export default function StudentTable(props){
             <td>Country</td>
             <td>Profile</td>
         </tr>
-
-        {students.map((student)=>(
-            <tr key={student.studentId}>
-             <td>{student.firstName}</td>
-             <td>{student.lastName}</td>
-             <td>{student.age}</td>  
-             <td>{student.course}</td>  
-             <td>{student.gender}</td>
-             <td>{student.address.country}</td>
-             <td>{student.skills}</td>
-             <td>{student.profilePic}</td>
-             </tr>
-        ))}
         </thread>
+        <tbody>
+           {
+            props.students.map( student=>
+                <tr>
+                    <td>{student.firstName}</td>
+                    <td>{student.lastName}</td>
+                    <td>{student.age}</td>  
+                    <td>{student.course}</td>  
+                    <td>{student.gender}</td>
+                    <td>{student.address.country}</td>
+                    <td>{student.skills}</td>
+                    <td>{student.profilePic}</td> 
+                </tr>
+            )
+           } 
+        </tbody>
+
+    
+       
     </table>
             </div>
 
