4a95323af81379c1dd12de15bc14859895bd9175
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-19 13:38:57 +0000

    base layout is created in APP

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 import  students  from './StudentDB';
 import  StudentTable from './StudentDB';
@@ -7,6 +8,14 @@ function App() {
   return (
     <div className="App">
      <h1>Students Information Portal</h1>
+     <hr/>
+     <table>
+      <tbody>
+       <tr>
+        <td></td>
+        </tr> 
+      </tbody>
+     </table>
     </div>
     </Student>
     </StudentTable/>
